eca194576a8e17f3d1369d84eaee825a2011dbce
Timothy-sserunjogi <sserunjogitimothy1221@gmail.com>
2020-07-07 16:11:37 +0300

    Test files have been added

---
+++ test/indexTest.js
@@ -0,0 +1,11 @@
+const assert = require("chai").asssert;
+const main = require("../index");
+
+describe("The registrationn login App", function () {
+    let name = tim;
+    // let password = pass; 
+
+    it("check for login credentials are true", function () {
+        assert.isString(name.firstName("tim"), "string");
+    })
+});
